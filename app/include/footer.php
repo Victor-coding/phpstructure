6a07f343f3e91c3f4b714cec80342d3b8707bc68
Adeptus <alecorn7641@gmail.com>
2023-05-07 13:05:02 +0300

    optimization structure ssite

---
+++ app/include/footer.php
@@ -0,0 +1,64 @@
+<footer class="container-fluid">
+    <div class="footer-content container">
+        <div class="row">
+            <div class="footer-section about col-md-4 col-12">
+                <h3 class="logo-text">Мой блог</h3>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    Lorem Ipsum is simply dummy text of the printing and
+                </p>
+                <div class="contact">
+                    <span><i class="fas fa-phone"> &nbsp; 123-456-789 </i></span>
+                    <span><i class="fas fa-envelope"> &nbsp; info@myblog.com</i></span>
+                </div>
+                <div class="socials">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+
+                </div>
+            </div>
+            <div class="footer-section links col-md-4 col-12">
+                <h3>Quick Links</h3>
+                <br>
+                <ul>
+                    <a href="">
+                        <li>События</li>
+                    </a>
+                    <a href="">
+                        <li>Команда</li>
+                    </a>
+                    <a href="">
+                        <li>Упражнения</li>
+                    </a>
+                    <a href="">
+                        <li>Галерея</li>
+                    </a>
+                    <a href="">
+                        <li>Что-то ещё</li>
+                    </a>
+                </ul>
+            </div>
+
+            <div class="footer-section contact-form col-md-4 col-12">
+                <h3>Контакты</h3>
+                <br>
+                <form action="/" method="post">
+                    <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+                    <textarea  name="message" rows="4" class="text-input contact-input" placeholder="Your message..."></textarea>
+                    <button type="submit" class="btn btn-big contact-btn">
+                        <i class="fas fa-envelope"></i>
+                        Отправить
+                    </button>
+                </form>
+            </div>
+
+        </div>
+
+        <div class="footer-bottom">
+            &copy; myblog.com | Designed my
+        </div>
+    </div>
+</footer>
+
